1ed56a7369c41bfbc1fc33a4ab4f2dfa2a9c3d6b
Bysantosj17 <uriel_barbosasan@hotmail.com>
2023-11-02 01:38:56 -0600

    02/11/2023 1:38am

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -8,20 +8,18 @@ use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
-    public function index($curso){
+    public function index(){
 
         $cursos = Curso::all();
 
-        return $cursos;
-
-        return view("cursos.index", compact ('cursos'));
+        return view('cursos.index', compact('cursos'));
     }
 
     public function create(){
-        return view("cursos.create");
+        return view('cursos.create');
     }
 
     public function show($curso){
-        return view("cursos.show", compact('curso'));
+        return view('cursos.show', compact('curso'));
     }
 }
--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -4,4 +4,9 @@
 
 @section('content')
     <h1>Bienvenido a la pagina cursos</h1>
+    <ul>
+        @foreach ($cursos as $curso)
+            <li>{{$curso->name}}</li>
+        @endforeach
+    </ul>
 @endsection
